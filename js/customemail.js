aa4e6ceb36d7d6ee6596eeae2e67e248df285a54
Vajid Kagdi <vajid.kagdi@sjsu.edu>
2019-05-14 12:48:57 -0700

    Update customemail.js

--- js/customemail.js
+++ js/customemail.js
@@ -17,7 +17,14 @@ function onReadyGeneral() {
     console.log(template_params)
     let service_id = "default_service";
     let template_id = "template_9rHUIb54";
+    
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    if(!(vars['email'] == "no")){
     emailjs.send(service_id, template_id, template_params);
+    } 
     localStorage.setItem('visited', "true");
     localStorage.setItem('visitedOn', new Date());
 
