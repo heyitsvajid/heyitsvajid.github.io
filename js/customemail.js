dc36768dad9b529fe3d89e3c63939437fc59a461
Vajid <vkagdi@paypal.com>
2019-08-20 13:04:26 -0700

    Resume download event added.

--- js/customemail.js
+++ js/customemail.js
@@ -14,7 +14,6 @@ function onReadyGeneral() {
         "visitedOn" : new Date(localStorage.getItem('visitedHeyitsvajidOn')).toLocaleString(),
         "message": JSON.stringify(data, null, 2)
     }
-    console.log(template_params)
     let service_id = "default_service";
     let template_id = "template_9rHUIb54";
     
@@ -27,4 +26,35 @@ function onReadyGeneral() {
     } 
     localStorage.setItem('visitedHeyitsvajid', "true");
     localStorage.setItem('visitedHeyitsvajidOn', new Date());
+}
+
+function openResume(){
+    if(localStorage.getItem("downloadedHeyitsvajidResumeOn")){
+        let previous = new Date(localStorage.getItem('downloadedHeyitsvajidResumeOn'))
+        let current = new Date()
+        let difference = current - previous
+        if(difference < 3600000){
+            return;
+        }
+    }
+    let denizen = new Denizen();
+    let data = denizen.getData();
+    var template_params = {
+        "downloaded": localStorage.getItem('downloadedHeyitsvajid'),
+        "downloadedOn" : new Date(localStorage.getItem('downloadedHeyitsvajidResumeOn')).toLocaleString(),
+        "message": JSON.stringify(data, null, 2)
+    }
+    let service_id = "default_service";
+    let template_id = "resume_download";
+
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    if(!(vars['email'] == "no")){
+        emailjs.send(service_id, template_id, template_params);
+    }
+    localStorage.setItem('downloadedHeyitsvajid', "true");
+    localStorage.setItem('downloadedHeyitsvajidResumeOn', new Date());
+    window.open('./Vajid_Kagdi_Resume.pdf', '_blank');
 }
